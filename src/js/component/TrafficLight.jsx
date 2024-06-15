f536e7588c97c145e71a46a7dab89578e12f7716
8u1137pr00f <162195925+8u1137pr00f@users.noreply.github.com>
2024-06-15 12:58:22 +0000

    3er update, añadidas las explicaciones dentro del TrafficLight.jsx para más comprensión del código

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,12 +1,13 @@
+//1.- Importaciones
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import '/workspaces/react-trafficlightJorge/src/styles/trafficLight.css';
-
+//2.- Defino el componente funcional TrafficLight
 const TrafficLight = () => {
     const [color, setColor] = useState("red");
     const colors = ["red", "yellow", "green"];
     const [extraColors, setExtraColors] = useState([]);
-
+//3.- Defino las funciones manejadoras de cada acción
     const handleColorClick = (selectedColor) => {
         setColor(selectedColor);
     };
@@ -20,7 +21,7 @@ const TrafficLight = () => {
     const addExtraColor = () => {
         setExtraColors([...extraColors, "purple"]);
     };
-
+//4.- Renderización del componente HTML
     return (
         <div className="traffic-light-container">
             <div className="traffic-light">
@@ -39,7 +40,7 @@ const TrafficLight = () => {
         </div>
     );
 };
-
+//5.- Renderización en el DOM
 ReactDOM.render(<TrafficLight />, document.querySelector('#app'));
-
+//6.- Exportación del componente funcional
 export default TrafficLight;
